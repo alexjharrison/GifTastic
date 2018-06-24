0776c0a378d1dd74ee1a67572eb75d4c006e45d8
Alex Harrison <alexjharrison@gmail.com>
2018-06-24 14:49:46 -0400

    more functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,11 @@
 // giphy api key 4k56iwIPS0RnWf5HeBblECm7wDCme0eb
 
 
-var shows = ["Buffy The Vampire Slayer", "Star Trek The Next Generation", "Person of Interest", "CSI Miami", "Gravity Falls", "Chopped", "House MD"];
+var shows = ["Buffy The Vampire Slayer", "Star Trek The Next Generation", "Person of Interest", "CSI:Miami", "Gravity Falls", "Iron Chef", "House"];
+var gifs = [];
+var gifThumbnail = [];
+var ratings = [];
+var gifTitle = [];
 
 $(document).ready(function () {
     var generateButtons = function () {
@@ -19,7 +23,55 @@ $(document).ready(function () {
     $(document).on("click", ".show-button", function () {
         var showInfoDiv = $(".show-info");
         showInfoDiv.empty();
-        showInfoDiv.show(400);
+        var showName = $(this).attr("data-button");
+        console.log(showName);
+        var omdbQuery = "https://www.omdbapi.com/?apikey=trilogy&type=series&t=" + showName;
+        $.ajax({
+            url: omdbQuery,
+            method: "GET"
+        }).then(function (response) {
+            var poster = $("<img>").attr({
+                src: response.Poster,
+                alt: showName + " Poster"
+            })
+            var posterDiv = $("<div>").append(poster).addClass("poster-div");
+            showInfoDiv.append(posterDiv);
+            var textInfoDiv = $("<div>");
+            textInfoDiv.addClass("text-info");
+            textInfoDiv.append($("<h2>").text(response.Title));
+            textInfoDiv.append($("<p>").text("Year: " + response.Year));
+            textInfoDiv.append($("<p>").text("Actors: " + response.Actors));
+            textInfoDiv.append($("<p>").text("Plot: " + response.Plot));
+            textInfoDiv.append($("<p>").text("Seasons: " + response.totalSeasons));
+            showInfoDiv.append(textInfoDiv);
+        })
+        var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + showName + "&api_key=4k56iwIPS0RnWf5HeBblECm7wDCme0eb&limit=10";
+        $.ajax({
+            url: giphyURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                gifs.push(response.data[i].images.fixed_height.url);
+                gifThumbnail.push(response.data[i].images.fixed_height_still.url);
+                ratings.push(response.data[i].rating);
+                gifTitle.push(response.data[i].title);
+            }
+            for (var i = 0; i < gifs.length; i++) {
+                var gifDiv = $("<div>").addClass("gif-div");
+                gifDiv.prepend($("<button>").text("Add to Favorites").attr("data-gifID",i).addClass("favorites").attr("download",""));
+                gifDiv.prepend($("<a href='"+gifs[i]+"' download><button>Download GIF</button></a>"));
+                gifDiv.prepend($("<p>").text("Rating: " + ratings[i]));
+                gifDiv.prepend($("<p>").text("Title: "+gifTitle[i]));
+                gifDiv.prepend($("<img>").attr({
+                    "src": gifThumbnail[i],
+                    "alt": gifTitle[i],
+                    "class": "gif-image still",
+                    "data-id": i
+                }));
+                $(".gif-list").prepend(gifDiv);
+            }
+        })
     })
     $(document).on("click", ".new-show", function () {
         $("header").animate({ opacity: .5 }, 500);
@@ -39,21 +91,29 @@ $(document).ready(function () {
             $("body").append(newForm);
             newForm.show(500);
             $(":input[name=new-show]").focus();
-            console.log("I'm run twice for some unknown reason");
         });
     });
-$(document).on("click", ".submit-button", function (event) {
-    event.preventDefault();
-    var movieName = $("#textInput").val().trim();
-    console.log(movieName);
-    shows.push(movieName);
-    $(".form").hide(500);
-    $("header, .container").animate({ opacity: 1 }, 500, function () {
-        generateButtons();
-        $(".form").remove();
+    $(document).on("click", ".submit-button", function (event) {
+        event.preventDefault();
+        var movieName = $("#textInput").val().trim();
+        console.log(movieName);
+        shows.push(movieName);
+        $(".form").hide(500);
+        $("header, .container").animate({ opacity: 1 }, 500, function () {
+            generateButtons();
+            $(".form").remove();
+        });
     });
-});
+    $(document).on("click",".gif-image",function() {
+        if($(this).hasClass("still")){
+            $(this).attr("src",gifs[$(this).attr("data-id")]).removeClass("still").addClass("animated");
+        }
+        else if($(this).hasClass("animated")) {
+            $(this).attr("src",gifThumbnail[$(this).attr("data-id")]).removeClass("animated").addClass("still");
+        }
+        
+    })
 
-//start page code
-generateButtons();
+    //start page code
+    generateButtons();
 })
